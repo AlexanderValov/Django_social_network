98035f2342c99bf8c6d16b03385ca87059f05645
AlexandrValov <alexvalov95@mail.ru>
2021-02-01 20:28:01 +0300

    'ver. 1.4'

--- bookmark/bookmark/urls.py
+++ bookmark/bookmark/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
 ]
--- bookmark/images/views.py
+++ bookmark/images/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+import redis
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -12,9 +14,22 @@ from common.decorators import ajax_required
 from actions.utils import create_action
 
 
+r = redis.Redis(host=settings.REDIS_HOST, 
+                        port=settings.REDIS_PORT, 
+                        db=settings.REDIS_DB)
+
 @login_required
 def images_list(request):
-    images = Image.objects.all()
+    by_created = '-created'
+    by_total_likes = '-total_liks'
+    by_title = '-title'
+    order_by = request.GET.get('order_by')
+    if order_by == None:
+        order_by = '-created'
+    direction = request.GET.get('direction')
+    ordering = order_by
+    images = Image.objects.order_by(ordering)
+    #images = Image.objects.all()
     paginator = Paginator(images, 8)
     page = request.GET.get('page')
     try:
@@ -31,7 +46,12 @@ def images_list(request):
         images = paginator.page(paginator.num_pages)
     context = {
         'section': 'images',
-        'images': images
+        'images': images,
+        'order_by': order_by,
+        'direction': direction,
+        'by_created': by_created,
+        'by_total_likes': by_total_likes,
+        'by_title': by_title
     }
     if request.is_ajax():
         return render(request, 'images/image/list_ajax.html', context)
@@ -85,8 +105,27 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Увеличиваем количество просмотров картинки на 1
+    total_views = r.incr('image:{}:views'.format(image.id))
+    # Увеличиваем рейтинг картинки на 1.
+    r.zincrby('image_ranking', image.id, 1)
     context = {
         'section': 'images',
-        'image': image
+        'image': image,
+        'total_views': total_views
     }
     return render(request, 'images/image/detail.html', context)
+
+@login_required
+def image_ranking(request):
+    # Получаем набор рейтинга картинок
+    image_ranking = r.zrange('image_ranking', 0, 1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Получаем отсортированый список самых популярный картинок
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    context = {
+        'section': 'images',
+        'most_viewed': most_viewed
+    }
+    return render(request, 'images/image/ranking.html', context)
